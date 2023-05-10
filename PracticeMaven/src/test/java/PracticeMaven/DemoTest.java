dd3ada95c2c5573d9d5b12d5cd002253e3808012
Bairaggithub <bairagiranajit1996@gmail.com>
2023-05-10 17:44:52 +0530

    1st

---
+++ PracticeMaven/src/test/java/PracticeMaven/DemoTest.java
@@ -0,0 +1,23 @@
+package PracticeMaven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver;
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in/");
+
+	}
+
+
+	}
+
+
